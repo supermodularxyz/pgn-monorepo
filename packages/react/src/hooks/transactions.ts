899a137b352238cc5befcc87e5258be67e9efa50
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-08-04 14:39:44 +0530

    Cleanup

--- packages/react/src/hooks/transactions.ts
+++ packages/react/src/hooks/transactions.ts
@@ -107,8 +107,7 @@ export function useFinalize() {
     l1AsSigner: true,
   });
 
-  return useMutation(async (hash: string) => {
-    console.log("finalize", hash, crossChainMessenger);
-    return crossChainMessenger?.finalizeMessage(hash).then((tx) => tx.wait());
-  });
+  return useMutation(async (hash: string) =>
+    crossChainMessenger?.finalizeMessage(hash).then((tx) => tx.wait())
+  );
 }
